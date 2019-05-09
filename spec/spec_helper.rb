b01ee71c7303c3366d0a40c04cacd97aa7bbc7f3
Tony Chia <tchia04@gmail.com>
2019-05-09 08:45:55 -0700

    Revert "Test pull request " (#2)
    * Revert "fix all rubocop issues"
    
    This reverts commit 6c1a5677d8606d1b3922bd64b7eb4e77dcb91de3.
    
    * Revert "fix one rubocop failure"
    
    This reverts commit f44d4aad5df40fe9f7ec22309ee51fb46b822b6c.
    
    * Revert "test rubocop failure"
    
    This reverts commit b5f6ec1f07946c5051c0ebbfae545c17018bb337.
    
    * Revert "restore gemspec file"
    
    This reverts commit ee6827580af0897c50c250307b47f9f813a5aa59.
    
    * Revert "this commit rake build fails"
    
    This reverts commit 891a957747fc8759f355bfdcd40d35e8bfde8c91.
    
    * Revert "restore everything"
    
    This reverts commit 17fef20b35a44bad025e3943fc75f7f76d06a0b0.
    
    * Revert "try unstable chrome"
    
    This reverts commit 0ff3c15e5c5dde6343337117ec6f0afad1aada04.
    
    * Revert "try 69.0.3497.100 again"
    
    This reverts commit a138a1bcdeb04dccca771bca3882a53e21d42c99.
    
    * Revert "restore"
    
    This reverts commit 05c363951b94338769e5de6e1a9d0e57b2c0701d.
    
    * Revert "mac works but ubuntu doesn't"
    
    This reverts commit 1456c75c1ef34c77c80276ac4a6b6eac5df4782a.
    
    * Revert "use google chrome 69.0.3497.100"
    
    This reverts commit 03500c1c96de1dbf61e5b0d3d197406752fc3d74.
    
    * Revert "use google-chrome-beta"
    
    This reverts commit 8f814cbf10abfd6625511c5c0e78a33bc9412e4f.
    
    * Revert "this should fail"
    
    This reverts commit 08d3135fe750bec61cfac71e67bb02287fa1adf0.
    
    * Revert "need to commit this for git ls-files to work"
    
    This reverts commit feab1a3b65fe1719fe14ada8e219465254a57044.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,7 +12,6 @@ RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.syntax = :expect
   end
-
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
