b5f6ec1f07946c5051c0ebbfae545c17018bb337
Tony Chia <tchia04@gmail.com>
2019-05-09 07:36:03 -0700

    test rubocop failure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,7 +12,10 @@ RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.syntax = :expect
   end
-  config.mock_with :rspec do |mocks|
+
+  config.mock_with :rspec do |mocks| 
     mocks.verify_partial_doubles = true
   end
 end
+
+
