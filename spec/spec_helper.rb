1ac0954162eaff98f4ef6a5e286d5fea5460360c
Tony Chia <tchia04@gmail.com>
2019-05-09 08:45:09 -0700

    Revert "test rubocop failure"
    This reverts commit b5f6ec1f07946c5051c0ebbfae545c17018bb337.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,10 +12,7 @@ RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.syntax = :expect
   end
-
-  config.mock_with :rspec do |mocks| 
+  config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
 end
-
-
